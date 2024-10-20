27145d9a3bef480213e5135f3d258deb3afed85d
EduardoBrito2 <eduardo.brito7@fatec.sp.gov.br>
2024-10-20 18:26:47 -0300

    atualização

--- aula10bd.sql
+++ aula10bd.sql
@@ -176,25 +176,43 @@ INSERT INTO tbl_empregados (nome, data_nascimento, endereco, sexo, salario, cod_
 ('Gustavo Gomes', '1986-09-25', 'Rua das Montanhas, 5432, Belo Horizonte, MG', 'M', 5700.00, 36),
 ('Lúcia Castro', '1990-05-19', 'Avenida das Árvores, 7890, Curitiba, PR', 'F', 5600.00, 36);
 
---1 crie uma view chamada v_depcidade que liste o nome de cada departamento com o nome da cidade onde este departamento está localizado.
+--1 crie uma view chamada v_depcidade que liste o nome de cada departamento com o nome da cidade onde este departamento está 
+--localizado.
 --Após a criacao executar Select * from v_depcidade; retorna 36 linhas
+create view v_depcidade as select nome(cidade) as cidade, nome(departamento) as departamento from tbl_cidades as cidade 
+inner join tbl_departamentos as departamento on departamento.cod_cidade = cidade.cod_cidade
+select * from v_depcidade
 
-
-
---2 crie uma view denominada v_depcidadehouston, a partir de v_depcidade que mostre somente os departamentos localizados em Houston
+--2 crie uma view denominada v_depcidadehouston, a partir de v_depcidade que mostre somente os departamentos 
+--localizados em Houston
 -- execute select * from v_depcidadehouston
-
+create view v_depcidadehouston as select * from v_depcidade where cidade = 'Houston'
+select * from v_depcidadehouston
 
 --3 Crie uma visão denominada de v_opsalario, a qual lista a soma e média de todos os salários dos empregados.
 -- execute select * from v_opsalario
-
+create view v_opsalario as select sum(salario), avg(salario) from tbl_empregados
+select * from v_opsalario
 
 --4 Criar a view vw_empregados_salarial que exibe o nome e o salário de todos os empregados com salário acima de 5500.
 -- execute select * from v_empregados_salarial
-
+create view v_empregados_salarial as select nome, salario from tbl_empregados
+where salario > 5500
+select * from v_empregados_salarial
 
 --5 criar uma view v_departemp para listar os departamentos e a quantidade de empregados em cada um
 -- listar os departamentos e as quantidades por orderm decrescente de quantidade.
+create view v_departemp(departamento,quantidade) as select nome(departamento), count(nome(empregados)) from tbl_departamentos as departamento
+inner join tbl_empregados as empregados on departamento.cod_departamento = empregados.cod_departamento
+group by nome(departamento) 
 
+select * from v_departemp order by quantidade desc
+-- drop view v_departemp
 
 --6 criar uma view v_departrouble para listar os nomes dos departamentos que possuem mais projetos do que empregados.
+create view v_departrouble (departamento) as select nome(departamentos) from tbl_departamentos as departamentos
+left join tbl_empregados as empregados on departamentos.cod_departamento = empregados.cod_departamento
+left join tbl_projetos as projeto on departamentos.cod_departamento = projeto.cod_departamento
+group by nome(departamentos)
+having count(projeto. *) > count(empregados. *)
+select * from v_departrouble
